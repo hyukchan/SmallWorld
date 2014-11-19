eb72604a93667aebad86f317bb0328a15602a432
Hyukchan <hyukchan.k@gmail.com>
2014-11-19 10:05:28 +0100

    adds unit test for Unit Class

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Small_World;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCreationOrcUnit()
+        {
+            OrcUnit orc = new OrcUnit();
+
+            Assert.AreEqual(orc.AttackPt, 2);
+            Assert.AreEqual(orc.DefensePt, 1);
+            Assert.AreEqual(orc.MovePt, 1);
+            orc.Move(new Position(1,2));
+            Assert.AreEqual(orc.MovePt, 0);
+        }
+    }
+}
