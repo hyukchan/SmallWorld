635a7d38158f649069cb01a2e600ae83c0fbf716
Hyukchan Kwon <hkwon@insa-rennes.fr>
2014-12-03 10:11:13 +0100

    Adds IHM WPF Project

---
+++ IHM/MapView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Drawing.BitmapImage;
+
+//TODO
+
+namespace IHM
+{
+    class MapView:Panel
+    {
+        protected override void OnRender(DrawingContext dc){
+            BitmapImage image = new BitmapImage(new Uri("textures/desert.png", UriKind.Relative));
+            dc.DrawImage("textures/desert.png",);
+        }
+    }
+}
