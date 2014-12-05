e8f2d962601ee1fc00f797e99ffd573b288b01ff
mallardflo <florent.mallard1@insa-rennes.fr>
2014-12-05 11:35:35 +0100

    Déplacement basique(possible ou non)

--- Algorithms/Algo.cpp
+++ Algorithms/Algo.cpp
@@ -5,16 +5,25 @@
 #include <time.h>
 using namespace std;
 
-#define PLAIN = 0
-#define DESERT = 1
-#define FOREST = 2
-#define MOUNTAIN = 3
+#define PLAIN 0
+#define DESERT 1
+#define FOREST 2
+#define MOUNTAIN 3
 
+#define INIT 0
+#define POSSIBLE 1
+#define IMPOSSIBLE 2
+
+/**
+* Constructeur
+*/
 Algo* Algo_new()
 {
 	return new Algo();
 }
-
+/**
+* Destructeur
+*/
 void delete_Algo(Algo* algo){
 	delete algo;
 }
@@ -79,4 +88,187 @@ int* startingPositions(int* map, int n){
 
 		return coordinates;
 }
+
+int* mapCreation(int size){
+	int* map = (int*)malloc(size * size * sizeof(int));
+	return map;
+}
+
+void freeMap(int* map){
+	free(map);
+}
+
+double* CostTab(int size)
+{
+	double* tab = (double*)malloc(size * size * sizeof(double));
+	return tab;
+}
+
+void freeCost(double* costTab)
+{
+	free(costTab);
+}
+
+/*
+
+Déplacements simples des unités
+
+*/
+
+//déplacements orc
+
+void orcMovement(int* map, double* cost, int size,int x, int y, double movPt, int* moves)
+{
+	double mvt = movPt;
+
+	switch (map[size*x + y])
+	{
+	case PLAIN :
+		if (mvt >= 0.5){
+			mvt = mvt - 0.5;
+			moves[x*size + y] = POSSIBLE;
+			cost[x*size + y] = mvt;
+		}
+		else{
+			moves[x*size + y] = INIT;
+		}
+		break;
+
+	case MOUNTAIN :
+		if (mvt >= 1){
+			mvt--;
+			moves[x*size + y] = POSSIBLE;
+			cost[x*size + y] = mvt;
+		}
+		else{
+			moves[x*size + y] = INIT;
+		}
+		break;
+	case FOREST :
+		if (mvt >= 1){
+			mvt--;
+			moves[x*size + y] = POSSIBLE;
+			cost[x*size + y] = mvt;
+		}
+		else{
+			moves[x*size + y] = INIT;
+		}
+		break;
+	case DESERT :
+		if (mvt >= 1){
+			mvt--;
+			moves[x*size + y] = POSSIBLE;
+			cost[x*size + y] = mvt;
+		}
+		else{
+			moves[x*size + y] = INIT;
+		}
+		break;
+	default :
+		break;
+	}
+
+
+}
+
+
+void elfMovement(int* map, double* cost, int size, int x, int y, double movPt, int* moves)
+{
+	double mvt = movPt;
+
+	switch (map[size*x + y])
+	{
+	case FOREST :
+		if (mvt >= 0.5){
+			mvt = mvt - 0.5;
+			moves[x*size + y] = POSSIBLE;
+			cost[x*size + y] = mvt;
+		}
+		else{
+			moves[x*size + y] = INIT;
+		}
+		break;
+
+	case MOUNTAIN:
+		if (mvt >= 1){
+			mvt--;
+			moves[x*size + y] = POSSIBLE;
+			cost[x*size + y] = mvt;
+		}
+		else{
+			moves[x*size + y] = INIT;
+		}
+		break;
+	case PLAIN :
+		if (mvt >= 1){
+			mvt--;
+			moves[x*size + y] = POSSIBLE;
+			cost[x*size + y] = mvt;
+		}
+		else{
+			moves[x*size + y] = INIT;
+		}
+		break;
+	case DESERT:
+			moves[x*size + y] = IMPOSSIBLE;
+		break;
+	default:
+		break;
+	}
+
+}
+
+void dwarfMovement(int* map, double* cost, int size, int x, int y, double movPt, int* moves)
+{
+	double mvt = movPt;
+
+	switch (map[size*x + y])
+	{
+	case PLAIN :
+		if (mvt >= 0.5){
+			mvt = mvt - 0.5;
+			moves[x*size + y] = POSSIBLE;
+			cost[x*size + y] = mvt;
+		}
+		else{
+			moves[x*size + y] = INIT;
+		}
+		break;
+
+	case MOUNTAIN:
+		if (mvt >= 1){
+			mvt--;
+			moves[x*size + y] = POSSIBLE;
+			cost[x*size + y] = mvt;
+		}
+		else{
+			moves[x*size + y] = INIT;
+		}
+		break;
+	case FOREST:
+		if (mvt >= 1){
+			mvt--;
+			moves[x*size + y] = POSSIBLE;
+			cost[x*size + y] = mvt;
+		}
+		else{
+			moves[x*size + y] = INIT;
+		}
+		break;
+	case DESERT:
+		if (mvt >= 1){
+			mvt--;
+			moves[x*size + y] = POSSIBLE;
+			cost[x*size + y] = mvt;
+		}
+		else{
+			moves[x*size + y] = INIT;
+		}
+		break;
+	default:
+		break;
+	}
+
+}
+// TODOSW Déplacement optimal
 
