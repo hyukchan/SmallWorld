6ce801f70635b9072be26d72c5e240b61b6e0ca2
mallardflo <florent.mallard1@insa-rennes.fr>
2015-01-07 14:11:25 +0100

    GameCreator

--- Small World/GameCreator.cs
+++ Small World/GameCreator.cs
@@ -7,20 +7,66 @@ namespace Small_World
 {
     public class GameCreator
     {
+        public const string DEMO = "Demo";
+        public const string MEDIUM = "Medium";
+        public const string LARGE = "Large";
+
+        public string gameType;
+        public GameBuilder gameBuilder;
+        public List<string> playerList;
+
         public GameBuilder GameBuilder
         {
             get
             {
-                throw new System.NotImplementedException();
+                return gameBuilder;
             }
             set
             {
+                gameBuilder = value;
             }
         }
 
-        public GameBuilder CreateGame()
+        public string GameType
         {
-            throw new NotImplementedException();
+            get
+            {
+                return gameType;
+            }
+            set
+            {
+                if (value == DEMO)
+                {
+                    gameType = value;
+                    GameBuilder = new DemoGameBuilder();
+                }
+                if (value == MEDIUM)
+                {
+                    gameType = value;
+                    GameBuilder = new MediumGameBuilder();
+                }
+                if (value == LARGE)
+                {
+                    gameType = value;
+                    GameBuilder = new LargeGameBuilder();
+                }
+            }
+        }
+
+        public GameCreator()
+        {
+            playerList = new List<string>();
+        }
+
+        public void AddPlayer(string name, string people)
+        {
+            playerList.Add(name);
+            playerList.Add(people);
+        }
+
+        public Game CreateGame()
+        {
+            return GameBuilder.CreateGame(playerList[0], playerList[1], playerList[2], playerList[3]);
         }
     }
 }
